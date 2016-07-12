d40410932853f711b53c74761e4f5611342e8975
Kirby Munson <kmunson@g.clemson.edu>
2016-07-12 14:32:31 -0400

    fixed toggle

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,23 @@
-alert('hello');
-
 // Pass jquery to IIFE as $ so we know $ is always = jQuery
 (function($){
   'use strict';
 
-    // Protect against DOM not being ready by wrapping code in jQuery callback
-    $(function(){
-      $('h1').text('Wow! jQuery is awesome');
 
-    });
+  $(document).ready(function(){
+      $("div.div1").click(function(){
+          $("div.1").toggleClass("main");
+      });
+  });
+  $(document).ready(function(){
+      $("div.div2").click(function(){
+          $("div.2").toggleClass("main1");
+      });
+  });
+  $(document).ready(function(){
+      $("div.div3").click(function(){
+          $("div.3").toggleClass("main2");
+      });
+  });
+
 
 }(jQuery));
